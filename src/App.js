6e67d19e1266de92f78ec2cd552a8b7f404529cf
Sthae <sthae38@gmail.com>
2021-02-14 18:19:03 +0200

    created basic components for the blog app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
+import Home from './components/home/Home';
+import Navbar from './components/nav/Navbar';
 
 function App()
 {
+
   return (
     <div className="App">
-      <h1>Hello there react</h1>
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+
+
+function Home()
+{
+    return (
+        <div className="home">
+            <h2>Homepage</h2>
+        </div>
+    );
+}
+
+export default Home;
