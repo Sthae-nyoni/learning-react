89c02833736e14868d19befa3e47c5e3a79417a5
Sthae <sthae38@gmail.com>
2021-02-14 20:28:49 +0200

    clean up

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 
 function App()
 {
+  function onClick(e)
+  {
+    console.log(e.target);
+  }
+
+  function onKeyboarEnter()
+  {
+    console.log("The enter key was pressed")
+  }
+
   return (
     <div className="App">
       <h1>Hello there react</h1>
+      <input onKeyPressCapture={onKeyboarEnter} type="text" />
+      <button onClick={e => onClick(e)}>Click me</button>
     </div>
   );
 }
