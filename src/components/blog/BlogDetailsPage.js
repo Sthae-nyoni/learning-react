f283c20db076b1613da78a4f3aa3cda5b212b983
Sthae <sthae38@gmail.com>
2021-02-17 23:26:01 +0200

    added a little styling for the blog details page

--- src/components/blog/BlogDetailsPage.js
+++ src/components/blog/BlogDetailsPage.js
@@ -8,7 +8,6 @@ function BlogDetailsPage()
     const { data: blog, pending, error_message } = useFetchService(url);
 
     return (
-
         <div className="blog-details">
             {pending && <div>Loading...</div>}
             {error_message && <div>{error_message}</div>}
@@ -24,7 +23,7 @@ function ArticleBody({ article_details })
         <div>
             <h2>Title: {article_details.title}</h2>
             <p>Writen by: {article_details.author}</p>
-            <p>{article_details.body}</p>
+            <div>{article_details.body}</div>
         </div>
     );
 }
